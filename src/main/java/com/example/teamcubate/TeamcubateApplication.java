689450f399c8708316bd0ec8bf577c8becb65c4e
vojislav.vulovic <vojislav.vulovic@hyperoptic.com>
2021-04-25 15:32:53 +0200

    initial commit

---
+++ src/main/java/com/example/teamcubate/TeamcubateApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamcubate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcubateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( TeamcubateApplication.class, args );
+	}
+
+}
